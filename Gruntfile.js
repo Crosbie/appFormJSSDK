c8e31c9f5fdf69d013ce83906ae8f7010dd53638
Keyang <keyang.xiang@feedhenry.com>
2013-12-04 10:47:43 +0000

    added link grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,13 @@
-var express=require("express");
-
-
 module.exports=function(grunt){
   grunt.initConfig({
-    pkg:grunt.file.readJSON("package.json")
+    pkg:grunt.file.readJSON("package.json"),
+    linkCore:{
+        "coreSrc":"<%= pkg.coreDir %>/",
+        "distFile":"<%= pkg.distDir %>/<%= pkg.distFile %>"
+    }
   });
 
   grunt.registerTask("testServer",require("./script/webServer.js"));
   grunt.registerTask("app",require("./script/appServer.js"));
+  grunt.registerTask("linkCore",require("./script/linkCore.js")(grunt));
 }
